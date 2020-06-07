d3759e72dde32c81c26a5d79a3ebe7da085af5c8
480814177 <480814177@qq.com>
2020-06-07 21:57:47 +0800

    feat：添加导航栏，跳转逻辑，数据展示

--- SwiftUI_App/MapView.swift
+++ SwiftUI_App/MapView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 import MapKit
 
 struct MapView : UIViewRepresentable {
+    // 地标坐标
+    var coordinate: CLLocationCoordinate2D
     
     typealias UIViewType = MKMapView
     // 创建mapView
@@ -19,7 +21,6 @@ struct MapView : UIViewRepresentable {
     
     // 配置view 响应事件，给 map view 设置坐标，
     func updateUIView(_ uiView: MapView.UIViewType, context: UIViewRepresentableContext<MapView>) {
-        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
         // 地图的宽和高
         let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
         let region = MKCoordinateRegion(center: coordinate, span: span)
@@ -31,7 +32,7 @@ struct MapView : UIViewRepresentable {
 #if DEBUG
 struct MapView_Previews : PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: landmarkData[0].locationCoordinate)
     }
 }
 #endif
