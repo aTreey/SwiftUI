4acce8806864cb8f999b61ee53ae208d4e754c78
于鸿鹏 <hongpeng03@gmail.com>
2020-08-18 23:12:46 +0800

    feat: 列表详情共享数据

--- SwiftUI_App/SceneDelegate.swift
+++ SwiftUI_App/SceneDelegate.swift
@@ -22,6 +22,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
+            
+            // 给 LandmarkList 添加 environmentObject(_:) 修饰符。
             window.rootViewController = UIHostingController(rootView: LandmarkList().environmentObject(UserData()))
             self.window = window
             window.makeKeyAndVisible()
