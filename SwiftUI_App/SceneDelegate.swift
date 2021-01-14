4cc9769dc40e397160de0a921dbaf613e9de6a69
480814177 <480814177@qq.com>
2021-01-14 14:14:02 +0800

    feat：添加自定义导航栏

--- SwiftUI_App/SceneDelegate.swift
+++ SwiftUI_App/SceneDelegate.swift
@@ -18,12 +18,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        // 添加环境变量
+//        let contentView = LandmarkList().environmentObject(UserData())
+        let contentView = HomeView().environmentObject(PostUserData())
 
         // Use a UIHostingController as window root view controller
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             // 给 LandmarkList 添加 environmentObject(_:) 修饰符。
-            window.rootViewController = UIHostingController(rootView: LandmarkList().environmentObject(UserData()))
+            window.rootViewController = UIHostingController(rootView: contentView)
             self.window = window
             window.makeKeyAndVisible()
         }
