c87d78d410fefd1abb5f57d8556dad21637f8ae4
aTreey <480814177@qq.com>
2022-11-08 00:35:25 +0800

    feat: 添加命名脚本

---
+++ rename.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+###
+# @Author: aTreey 480814177@qq.com
+# @Date: 2022-11-08 00:12:39
+# @LastEditors: aTreey 480814177@qq.com
+# @LastEditTime: 2022-11-08 00:34:41
+# @FilePath: /undefined/Users/yuhongpeng/Dev/LoginOC/changeCommitMsg.sh
+# @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+###
+
+git filter-branch --env-filter '
+
+# 之前的邮箱
+OLD_EMAIL="hongpeng03@gmail.com"
+# 修改后的用户名
+CORRECT_NAME="480814177"
+# 修改后的邮箱
+CORRECT_EMAIL="480814177@qq.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
